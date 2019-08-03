97ee757453122e5b404cd8fcce7012ac7e19e636
drcrotato <31459944+drcrotato@users.noreply.github.com>
2019-08-03 14:36:11 -0400

    general bug fixing
    -fixed bugs with collision detection
    -added in some extra data that shows in a small box in the corner

--- main.js
+++ main.js
@@ -18,20 +18,29 @@ const player = {
         y2: undefined,
         x3: undefined,
         y3: undefined,
+    },
+    start: {
+        x: 0,
+        y: 0,
+        // onchange html automatically converts degrees to radians
+        // where 0 degrees is pointing towards the top of the screen
+        rot: Math.PI / 2, // start at 90 degrees or pointing forward
     }
 };
 let mouse = {
     x1: undefined,
     y1: undefined,
     x2: undefined,
-    y2: undefined
+    y2: undefined,
+    goal: undefined
 };
-const lines = [];
+let lines = [];
 // drawOn is the variable that tells what drawOn the player is in
 // if in mode drawOn (aka true) then draw the lines of the mouse
 // otherwise don't 
 let drawOn = true;
-
+let drawGoal = false;
+let checkForUndo = true;
 setup();
 
 function setup() {
@@ -69,6 +78,7 @@ function setup() {
         if (drawOn == true) {
             mouse.x1 = ev.clientX - (document.body.clientWidth / 2);
             mouse.y1 = -ev.clientY + (document.body.clientHeight / 2);
+            mouse.goal = drawGoal;
         }
     });
     window.addEventListener("mousemove", (ev) => {
@@ -79,17 +89,21 @@ function setup() {
         }
     })
     window.addEventListener("mouseup", (ev) => {
-        if (drawOn == true) {
+        if (drawOn == true && mouse.x1 != undefined && mouse.x2 != undefined) {
             mouse.x2 = ev.clientX - (document.body.clientWidth / 2);
             mouse.y2 = -ev.clientY + (document.body.clientHeight / 2);
             // push current mouse coords into lines array
-            if (mouse.x1 != mouse.x2) {
+            if ((mouse.x1 == mouse.x2 && mouse.y1 == mouse.y2) == false) {
                 lines.push({
                     x1: mouse.x1,
                     y1: mouse.y1,
                     x2: mouse.x2,
-                    y2: mouse.y2
+                    y2: mouse.y2,
+                    goal: drawGoal,
                 });
+                // update the lines array showing in the box
+                let txt = document.querySelector("#lines");
+                txt.innerHTML = lines.toSource();
             }
         }
         // clear mouse coord
@@ -97,7 +111,7 @@ function setup() {
         mouse.y1 = undefined;
         mouse.x2 = undefined;
         mouse.y2 = undefined;
-    })
+    });
     window.requestAnimationFrame(draw);
 }
 
@@ -109,41 +123,59 @@ function draw() {
     // clear canvas
     ctx.fillStyle = "white";
     ctx.fillRect(-c.width / 2, -c.height / 2, c.width, c.height);
+    // updates position
+    player.x += player.vx;
+    player.y += player.vy;
+    //simulate friction/gravity/air resistance
+    player.vx *= 85 / 100;
+    player.vy *= 85 / 100;
+    // put position info in the box
+        document.getElementById("position").innerHTML = `x: ${player.x.toPrecision(2)} y: ${player.y.toPrecision(2)} rot: ${((180 * (player.rot/Math.PI)).toPrecision(2) % 360)-90}`;
     // draws player
     drawPlayer();
-    // draws the line that the player is currently drawing
+    // draws all the lines in array lines and does collision detection
+    let stopDetecting = false;
     for (let i = 0; i < lines.length; i++) {
+        // draw the line
         drawLine(lines[i]);
+        // get all of the player vertices and make them into the lines of the triangle
         let pv = player.vertices;
         let line1 = {x1:pv.x1,y1:pv.y1,x2:pv.x2,y2:pv.y2};
         let line2 = {x1:pv.x2,y1:pv.y2,x2:pv.x3,y2:pv.y3};
         let line3 = {x1:pv.x3,y1:pv.y3,x2:pv.x1,y2:pv.y1};
         // console.log(intercepting(line1,lines[i]).b);
-        if (intercepting(line1,lines[i]).b == true
+        if ((intercepting(line1,lines[i]).b == true
         || intercepting(line2,lines[i]).b == true
-        || intercepting(line3,lines[i]).b == true) {
-            console.log("COLLISION!!!!!!!!");
+        || intercepting(line3,lines[i]).b == true)
+        && stopDetecting == false) {
+            if (lines[i].goal == true) {
+                collisionHandler(true);
+                console.log("YOU WON!!!!");
+            } else {
+                collisionHandler(false);
+                console.log("COLLISION!!!!!!!!");
+            }
+            stopDetecting = true;
         }
     }
-    // updates position
-    player.x += player.vx;
-    player.y += player.vy;
-    //simulate friction/gravity/air resistance
-    player.vx *= 19 / 20;
-    player.vy *= 19 / 20;
-    // draw mouse line
+    // draws the line that the player is currently drawing
     drawLine({
         x1: mouse.x1,
         y1: mouse.y1,
         x2: mouse.x2,
-        y2: mouse.y2
+        y2: mouse.y2,
+        goal: mouse.goal
     });
 }
 
 function drawLine(line) {
-    let e = line;
     ctx.beginPath();
-    ctx.strokeStyle = "black";
+    if (line.goal == true) {
+        ctx.strokeStyle = "green";
+    } else {
+        ctx.strokeStyle = "black";
+    }
+    let e = line;
     ctx.moveTo(e.x1, e.y1);
     ctx.lineTo(e.x2, e.y2);
     ctx.stroke();
@@ -160,6 +192,7 @@ function drawPlayer() {
     pv.y2 = player.y + (10 * Math.sin(player.rot + (140 * (Math.PI / 180))));
     pv.x3 = player.x + (10 * Math.cos(player.rot + (220 * (Math.PI / 180))));
     pv.y3 = player.y + (10 * Math.sin(player.rot + (220 * (Math.PI / 180))));
+    // draw that triangle
     ctx.beginPath();
     ctx.fillStyle = "black";
     ctx.moveTo(pv.x1, pv.y1);
@@ -168,20 +201,53 @@ function drawPlayer() {
     ctx.closePath();
     ctx.fill();
 }
-
+// checks to see what key combinations are pressed down at the time
 function checkCombinations() {
     if (keys.find((v) => v == "w")) {
-        player.vx += Math.cos(player.rot);
-        player.vy += Math.sin(player.rot);
+        player.vx += (1) * Math.cos(player.rot);
+        player.vy += (1) * Math.sin(player.rot);
     }
     if (keys.find((v) => v == "s")) {
-        player.vx -= Math.cos(player.rot);
-        player.vy -= Math.sin(player.rot);
+        player.vx -= (1/2) * Math.cos(player.rot);
+        player.vy -= (1/2) * Math.sin(player.rot);
     }
     if (keys.find((v) => v == "a")) {
         player.rot += 5 * (Math.PI / 180);
     }
     if (keys.find((v) => v == "d")) {
-        player.rot -= 3 * (Math.PI / 180);
+        player.rot -= 5 * (Math.PI / 180);
+    }
+    if (keys.find((v) => v == "Control") && keys.find((v) => v =="z") && checkForUndo == true) {
+        lines.pop();
+        checkForUndo = false;
+        setTimeout(()=>{checkForUndo = true}, 500);
+    }
+}
+// handles the collision based on whether or not it's hitting the goal
+function collisionHandler(goal) {
+    if (goal == true) {
+
+    } else {
+        player.x = player.start.x;
+        player.y = player.start.y;
+        player.rot = player.start.rot;
+        player.vx = 0;
+        player.vy = 0;
+    }
+}
+// handles when button to show and hide line data is clicked
+let lineDataShown = false;
+function toggleLineData() {
+    let txt = document.querySelector("#lines");
+    let button = document.querySelector("#toggle-line-data");
+    if (lineDataShown == false) {
+        txt.innerHTML = lines.toSource();
+        button.innerHTML = "Show Line Data";
+        txt.hidden = true;
+        lineDataShown = true;
+    } else {
+        txt.hidden = false;
+        button.innerHTML = "Hide Line Data";
+        lineDataShown = false;
     }
 }
--- math.js
+++ math.js
@@ -1,36 +1,91 @@
 function getFormula(line) {
-    let slope = (line.y2 - line.y1)/(line.x2 - line.x1);
-    // y = mx + b
-    // y = slope * x + b
-    // y1 = slope * x1 + b
-    // y1 - slope * x1 = b
-    let intercept = line.y1 - (line.x1 * slope);
+    let intercept;
+    let slope;
+    if (line.x1 == line.x2) {
+        slope = null;
+        intercept = line.x1;
+    } else {
+        slope = (line.y2 - line.y1)/(line.x2 - line.x1);
+        // y = mx + b
+        // y = slope * x + b
+        // y1 = slope * x1 + b
+        // y1 - slope * x1 = b
+        intercept = line.y1 - (line.x1 * slope);
+    }
     return {m: slope, b: intercept};
 }
 
 function intercepting(line1,line2) {
     let intercept = true;
     // get formulas of both lines
-    let l1 = getFormula(line1);
-    let l2 = getFormula(line2);
-    // check their intersection
-    // l1.m * x + l1.b = l2.m * x + l2.b
-    // (l1.m - l2.m) * x = l2.b - l1.b
-    let x = (l2.b - l1.b)/(l1.m - l2.m);
-    // bring x back in
-    let y = l1.m * x + l1.b;
+    let l1 = getFormula(line1),
+    l2 = getFormula(line2);
+    let x,y;
+    if (l1.m == null) {
+        // let x = the x intercept
+        x = l1.b;
+        // bring x into
+        // y = l2.m * x + l2.b
+        y = l2.m * x + l2.b;
+        // define ranges and detect if x and/or y is out of the ranges
+        let minY1 = Math.min(line1.y1,line1.y2,y),
+        maxY1 = Math.max(line1.y1,line1.y2,y),
+        minX2 = Math.min(line2.x1,line2.x2,x),
+        maxX2 = Math.max(line2.x1,line2.x2,x),
+        minY2 = Math.min(line2.y1,line2.y2,y),
+        maxY2 = Math.max(line2.y1,line2.y2,y);
+        // if the intersecting y is within both of the lines max and min y
+        // and if the intersecting x is within the 2nd line intercept is true
+        if (minY1 == y || maxY1 == y || minX2 == x
+        || maxX2 == x || minY2 == y || maxY2 == y) {
+            intercept = false;
+        }
+    } else if (l2.m == null) {
+        // let x = the x intercept
+        x = l2.b;
+        // bring x into
+        // y = l2.m * x + l2.b
+        y = l1.m * x + l1.b;
+        // define ranges and detect if x and/or y is out of the ranges
+        let minX1 = Math.min(line1.x1,line1.x2,x),
+        maxX1 = Math.max(line1.x1,line1.x2,x),
+        minY1 = Math.min(line1.y1,line1.y2,y),
+        maxY1 = Math.max(line1.y1,line1.y2,y),
+        minY2 = Math.min(line2.y1,line2.y2,y),
+        maxY2 = Math.max(line2.y1,line2.y2,y);
+        // if the intersecting y is within both of the lines max and min y
+        // and if the intersecting x is within the 2nd line intercept is true
+        if (minX1 == x || maxX1 == x || minY1 == y
+        || maxY1 == y || minY2 == y || maxY2 == y) {
+            intercept = false;
+        }
+    } else if ((l1.m == null && l2.m == null) || l1.m == l2.m) {
+        if (l1.b == l2.b) {
+            intercept = true;
+        } else {
+            intercept = false;
+        }
+    } else {
+        // check their intersection
+        // l1.m * x + l1.b = l2.m * x + l2.b
+        // (l1.m - l2.m) * x = l2.b - l1.b
+        x = (l2.b - l1.b)/(l1.m - l2.m);
+        // bring x back in
+        y = l1.m * x + l1.b;
+        // define ranges and detect if x is out of the ranges
+        let minX1 = Math.min(line1.x1,line1.x2,x),
+        maxX1 = Math.max(line1.x1,line1.x2,x),
+        minX2 = Math.min(line2.x1,line2.x2,x),
+        maxX2 = Math.max(line2.x1,line2.x2,x);
+        if (minX1 == x || maxX1 == x || minX2 == x || maxX2 == x) {
+            intercept = false;
+        }
+    }
     // for testing draw intersecting points
     // ctx.beginPath();
     // ctx.arc(x,y,10,0,2*Math.PI);
     // ctx.closePath();
     // ctx.fill();
-    //
-    let minX1 = Math.min(line1.x1,line1.x2,x),
-    maxX1 = Math.max(line1.x1,line1.x2,x),
-    minX2 = Math.min(line2.x1,line2.x2,x),
-    maxX2 = Math.max(line2.x1,line2.x2,x);
-    if (minX1 == x || maxX1 == x || minX2 == x || maxX2 == x) {
-        intercept = false;
-    }
+    
     return {b:intercept,x:x,y:y};
 }
